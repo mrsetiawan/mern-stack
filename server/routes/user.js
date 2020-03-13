df1f2e49e60087999a658c23f8277b5ad3084ff6
rahmat <muhamadrahmatsetiawan@gmail.com>
2020-03-14 02:12:37 +0700

    add router get user

---
+++ server/routes/user.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+let User = require('../model/user.model.js');
+
+router.route('/').get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error:' + err));
+});
--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 7000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongooDB connection estabilished successfully");
+})
+
+const exerciseRouter = require('./routes/exercise.js');
+const userRouter = require('./routes/user.js');
+
+app.use('/exercise', exerciseRouter);
+app.use('/user', userRouter);
 
 app.listen(port, () => {
   console.log(`server running in ${port}`);
